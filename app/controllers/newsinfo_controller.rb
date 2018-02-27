fa6c24b5b4e8c40d0a713299e0fdda5ab4c271f9
Sungheek <klpoik@naver.com>
2018-02-27 08:50:50 +0000

    crolwer update

--- app/controllers/newsinfo_controller.rb
+++ app/controllers/newsinfo_controller.rb
@@ -41,6 +41,74 @@ class NewsinfoController < ApplicationController
   end
 
   def crolling
+    # @article = Hash.new
     
+    @titles = Array.new
+    @press = Array.new
+    @wdate = Array.new
+    @url = Array.new
+    
+    companycode = "005930"
+    url = "http://finance.naver.com/item/news_news.nhn?code=005930"
+    naver_html = HTTParty.get(url)
+    doc = Nokogiri::HTML(naver_html)
+    
+    # 해쉬 key value로 구분하여 크롤링을 할려했지만 모든 아티클의 타이틀 값들이 키로 들어가버림 ! 
+    # doc.css("body > div > table.type5 > tbody").each do |article|
+    #   @article[article.css("tr > td.title").text] = {
+    #     :title => article.css("tr > td.title").text,
+    #     # summary: article.css("dd.articleSummary")[0].text,
+    #     :press => article.css("tr > td.info").text,
+    #     # :bar => article.css("dd.articleSummary > span.bar").text,
+    #     :wdate => article.css("tr > td.date").text
+    #     # :url => article.css("td.title > a")[article.length]["href"]
+    #   }
+    # end
+    
+    # puts @article.inspect
+    
+    # 타이틀 
+    doc.css("body > div > table.type5 > tbody > tr > td.title > a").each do |titles|
+      @titles << titles.text
+    end
+    puts @titles.inspect
+    
+    # 정보제공 
+    doc.css("body > div > table.type5 > tbody > tr > td.info").each do |press|
+      @press << press.text
+    end
+    puts @press.inspect
+    
+    # 날짜 
+    doc.css("body > div > table.type5 > tbody > tr > td.date").each do |wdates|
+      @wdate << wdates.text
+    end
+    
+    puts @wdate.inspect
+    # url 
+    doc.css("body > div > table.type5 > tbody > tr > td.title").each do |urls|
+      @url << urls.css("a")[0]['href']
+    end
+    
+    puts @url.inspect
+    
+    # article id
+    @article_id = Array.new
+    
+    @url.each do |id|
+      @article_id << id[31,10]
+    end
+    
+    puts @article_id
+    
+    # office id
+    @office_id = Array.new
+    
+    @url.each do |id|
+      @office_id << id[52,3]
+    end
+    # @office_id = @url_test[31,10]
+    
+    puts @office_id
   end
 end
